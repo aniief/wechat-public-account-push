4fa127bd5dec10de39c85f3472e8240b78e59735
aniie <43487498+aniief@users.noreply.github.com>
2023-10-10 22:25:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,18 +12,18 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
-
+  USE_PASSAGE: 'push-deer',
   PROVINCE: '广东',
   CITY: '惠州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'DEER',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU25933TGM71tcNGtEjEbAVis418l3Y0mi497NXo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
